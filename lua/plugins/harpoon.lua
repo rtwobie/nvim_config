98a50c8efc28f14185d2692d3d08270263c1a01e
unknown <robeats.hale@gmail.com>
2024-11-21 05:22:37 +0100

    fix: works now

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,19 +1,27 @@
 return {
-	'ThePrimeagen/harpoon',
+	"ThePrimeagen/harpoon",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
 		local map = function(keys, func, desc, mode)
-			mode = mode or 'n'
-			vim.keymap.set(mode, keys, func, { desc = 'harpoon: ' .. desc })
+			mode = mode or "n"
+			vim.keymap.set(mode, keys, func, { desc = "harpoon: " .. desc })
 		end
 
-		map("<leader>hm", require('harpoon.mark').add_file, "mark file")
-		map("<leader>ho", require('harpoon.ui').toggle_quick_menu, "open menu")
-		map("<C-h>", function() require('harpoon.ui').nav_file(1) end, "open file 1")
-		map("<C-j>", function() require('harpoon.ui').nav_file(2) end, "open file 2")
-		map("<C-k>", function() require('harpoon.ui').nav_file(3) end, "open file 3")
-		map("<C-l>", function() require('harpoon.ui').nav_file(4) end, "open file 4")
-	end
+		map("<leader>hm", require("harpoon.mark").add_file, "mark file")
+		map("<leader>ho", require("harpoon.ui").toggle_quick_menu, "open menu")
+		map("<M-1>", function()
+			require("harpoon.ui").nav_file(1)
+		end, "open file 1")
+		map("<M-2>", function()
+			require("harpoon.ui").nav_file(2)
+		end, "open file 2")
+		map("<M-3>", function()
+			require("harpoon.ui").nav_file(3)
+		end, "open file 3")
+		map("<M-4>", function()
+			require("harpoon.ui").nav_file(4)
+		end, "open file 4")
+	end,
 }
