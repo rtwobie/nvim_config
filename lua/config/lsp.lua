c2bcff778ccb96675275dd3266ceb720e680a1b3
9S <robeats.hale@gmail.com>
2026-03-19 12:10:24 +0100

    trying different built-in autocompletion function

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,3 +1,15 @@
+vim.lsp.config('*', {
+	on_attach = function(client, bufnr)
+        vim.lsp.completion.enable(true, client.id, bufnr, {
+            autotrigger = true,
+            convert = function(item)
+                return { abbr = item.label:gsub("%b()", "") }
+            end,
+        })
+        -- Example custom keybind for triggering autocompletion
+        -- vim.keymap.set("i", "<C-space>", vim.lsp.completion.get, { desc = "trigger autocompletion" })
+    end
+})
 -- Display Diagnostic Messages
 vim.diagnostic.config({
 	-- Use either virtual_text diagnostics in the line
